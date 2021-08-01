91d1200cf256438b07c120ede75d32600f07bb92
jramirez <ramirez.jocelyn73@yahoo.com>
2021-07-31 23:21:51 -0700

    commit tree 18e33ce7cbc2edefad1d6e988cdbee90f23f6379

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(driv, firstKey, val){
+    const updatedDrivers = {...driv}
+    updatedDrivers[firstKey] = val
+    return updatedDrivers
+
+//   return  Object.assign({}, driv, {firstKey:val}) this no work b/c sees firstKey as an actual name key
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driv, firstKey, val){
+    driv[firstKey] = val 
+    return driv 
+}
+function deleteFromDriverByKey(driv, firstKey){
+    const copyDriver = {... driv}
+    delete copyDriver[firstKey]
+    return copyDriver
+}
+
+function destructivelyDeleteFromDriverByKey(driv, firstKey){
+    delete driv[firstKey]
+    return driv
+}
